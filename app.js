d459f831429c75dfa678300138fe27c0675300c9
Ashish Barnawal <dark32@homePC>
2020-05-29 19:12:40 +0530

    before mac

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var rug = require('random-username-generator');
 
 var PORT = 3000;
 
+var users = [];
+var DRAW_ITEMS = ["Ice Cream", "Sandwich", "House", "Cage", "Necklace", "Piano", "Mobile", "Headphones"];
+var drawer_index = 0;
+var GAME_DURATION = 10;
+
 //for testing only!!
 app.use(express.static(__dirname));
 
@@ -19,18 +24,81 @@ server.listen(PORT, () => {console.log(`listening on ${PORT}`)});
 var sock2user = new Map();
 
 
+var gameTimeout;
+var gameInterval; //gameinfo interval
+var drawItem;
+var drawer;
+var timeleft;
+function startGame(){
+    if(users.length == 0) return;
+    drawer_index += 1;
+    drawer_index %= users.length;
+    drawItem = DRAW_ITEMS[Math.floor(Math.random()*2.1*DRAW_ITEMS.length)%DRAW_ITEMS.length];
+
+    drawer = users[drawer_index];
+    io.emit('game start', {'drawer': sock2user.get(drawer), 'timeRemaining':GAME_DURATION});
+    io.to(drawer).emit('drawer', drawItem);
+    console.log('GAME START\n=====');
+    console.log(`ITEM: ${drawItem}`);
+    console.log('DRAWER:\t' + drawer + '\t' + sock2user.get(drawer));
+    gameTimeout = setTimeout(endGame, GAME_DURATION*1000);
+    timeleft = GAME_DURATION;
+    gameInterval = setInterval(updateInfo, 1000);
+}
+
+function updateInfo(){
+    io.emit('game info', {'drawer': sock2user.get(drawer), 'timeRemaining': timeleft});
+    timeleft -= 1;
+}
+
+function endGame(){
+    console.log("=====\nGAME ENDED")
+
+    //show end game page
+    clearInterval(gameInterval);
+    clearTimeout(gameTimeout);
+    if(users.length > 0) startGame();
+}
+
 io.on('connection', (socket) => {
+    users.push(socket.id);
+
     var username = rug.generate();
-    console.log('new user connected : socket = ' + socket.id + ' username ' + username);
-    socket.emit('username', username);
-    sock2user.set(socket.id, username);
-           
-    socket.on('disconnect', () =>{
-        sock2user.delete(socket.id);
-    });
+
     socket.on('username', (newusrname) => {
-        console.log(socket.id + ': username : ' + newusrname);
+        if(users.includes(newusrname)) return;
         sock2user.set(socket.id, newusrname);
         socket.emit('username', newusrname);
+        console.log('user name chng:\t' + socket.id + '\t' + newusrname);
     });
+
+    socket.on('update canvas', (newcanvas) => {
+        if(socket.id != drawer) return;
+        socket.broadcast.emit('update canvas', newcanvas);
+    });
+
+
+    socket.on('disconnect', () =>{
+        // TODO:  if drawer disconnected then restart game
+
+        console.log('disconnected:\t' + socket.id + '\t' + sock2user.get(socket.id));
+        users = users.filter((x) => { console.log('x = ' + x + ` x!=${socket.id} : ` + x != socket.id); return x != socket.id;});
+        console.log(JSON.stringify(users));
+        sock2user.delete(socket.id);
+
+        if(users.length == 0 || drawer == socket.id){
+            endGame();
+        }
+    });
+
+    console.log('num of users  :\t', users.length);
+    console.log('user connected:\t' + socket.id + '\t' + username);
+    socket.emit('username', username);
+    sock2user.set(socket.id, username);
+
+    if(users.length==1){
+        startGame();
+    }
+
 });
+
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var disabled = true;
-
+var canvasDisabled = true;
+const socket = io();
 
 window.addEventListener('load', function (){
     var canvas = document.getElementById('mainCanvas');
@@ -13,21 +13,21 @@ window.addEventListener('load', function (){
     document.addEventListener('keypress', canvasClear);
 
     function canvasClear(e){
-        if(disabled) return;
+        if(canvasDisabled) return;
         if(e.key=='C' || e.key=='c'){
             ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
     }
 
     function setPosition(e){
-        if(disabled) return;
+        if(canvasDisabled) return;
         let rect = canvas.getBoundingClientRect();
         pos.x = e.clientX - rect.left;
         pos.y = e.clientY - rect.top;
     }
 
     function draw(e){
-        if(disabled) return;
+        if(canvasDisabled) return;
         if(e.buttons != 1) return;
         ctx.beginPath();
         ctx.lineWidth = 5;
@@ -38,5 +38,70 @@ window.addEventListener('load', function (){
         ctx.lineTo(pos.x, pos.y);
 
         ctx.stroke();
+        socket.emit('update canvas', canvas.toDataURL());
     };
+
+
+    function updateMessage(msg){
+        document.getElementById('game-message').innerHTML = msg;
+    }
+    function updateTimeRemaining(time){
+        document.getElementById('timeleft').innerText = `${Math.floor(time/60)}:${time%60}`;
+    }
+
+    socket.on('username', (usr) => {
+        console.log('username:' + usr);
+        document.getElementById('username').innerText = usr;
+    });
+
+    socket.on('game start', (gameinfo) => {
+        console.log('game start');
+        /*
+        1. clear canvas
+        2. update game message
+        4. set time remaining
+        */
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        updateMessage(gameinfo['drawer'] + ' is drawing');
+        updateTimeRemaining(gameinfo['timeRemaining']);
+
+    });
+
+
+    socket.on('game info', (gameinfo) => {
+        //console.log('game info' + JSON.stringify(gameinfo));
+        updateTimeRemaining(gameinfo['timeRemaining']);
+        
+        if(gameinfo['drawer'] != document.getElementById('username').innerText)
+            updateMessage(gameinfo['drawer'] + ' is drawing');
+    });
+
+    socket.on('drawer', (item) => {
+        console.log('I need to draw : ' + item);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        updateMessage("YOU need to draw : " + item);
+        canvasDisabled = false;
+    });
+
+    socket.on('update canvas', (canvasDataURL) => {
+        console.log('update canvas');
+        let img = new Image;
+        img.onload = () => ctx.drawImage(img,0,0);
+        img.src = canvasDataURL;
+    });
+
+
+    $('#chatbox-form').submit((e) => {
+        e.preventDefault();
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    $('#username-form').submit((e) => {
+        e.preventDefault();
+        socket.emit('username', $('#username-input').val());
+        $('#username-input').val('');
+        return false;
+    });
+
 });
